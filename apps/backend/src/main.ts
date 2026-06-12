a22097a9cc4786520760a4f028d4e4de8d018c11
Eslam osama <eslam.osama@exas2030.com>
2026-06-12 21:51:52 +0300

    back: implement a dummy url end point

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(4000)
+  console.log('Backend running on http://localhost:4000')
+}
+
+bootstrap().catch(console.error)

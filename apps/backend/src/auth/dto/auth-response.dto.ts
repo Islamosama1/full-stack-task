9ee6fce972d97fa8c2232418c288fa9b0501afe9
Eslam osama <eslam.osama@exas2030.com>
2026-06-13 13:32:22 +0300

    back: add the dtos of response and login

---
+++ apps/backend/src/auth/dto/auth-response.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger'
+
+export class UserProfileDto {
+  @ApiProperty() id!: string
+  @ApiProperty() name!: string
+  @ApiProperty() email!: string
+  @ApiProperty() createdAt!: Date
+}
+
+export class AuthResponseDto {
+  @ApiProperty() accessToken!: string
+  @ApiProperty({ type: UserProfileDto }) user!: UserProfileDto
+}

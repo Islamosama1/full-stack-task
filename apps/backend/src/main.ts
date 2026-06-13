23eee2bf4b9715bee10e9607d102c81ba735b368
Eslam osama <eslam.osama@exas2030.com>
2026-06-13 14:19:21 +0300

    back: add CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,12 +1,19 @@
 import 'reflect-metadata'
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+
+  app.enableCors({
+    origin: configService.getOrThrow<string>('CORS_ORIGIN'),
+    credentials: true,
+  })
 
   app.useGlobalFilters(new GlobalExceptionFilter())
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }))

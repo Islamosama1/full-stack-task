8e673f1a52a658efbd7e67975a201d891bda75a8
Eslam osama <eslam.osama@exas2030.com>
2026-06-13 13:17:06 +0300

    back: apply repository design pattern , so we separate database logic from the bussiness logic

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from './schemas/user.schema'
+import { UserRepository } from './repositories/user.repository'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [UserRepository, AuthService],
 })
 export class AuthModule {}

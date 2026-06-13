a63cc0bff56ab4495c340a010ebfb8716f4cccf6
Eslam osama <eslam.osama@exas2030.com>
2026-06-13 19:15:33 +0300

    back: implement coockies

--- apps/backend/src/auth/auth-cookie.service.ts
+++ apps/backend/src/auth/auth-cookie.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { Response } from 'express'
-import ms, { StringValue } from 'ms'
+import ms from 'ms'
 
 const COOKIE_NAME = 'accessToken'
 
@@ -10,7 +10,8 @@ export class AuthCookieService {
   constructor(private readonly configService: ConfigService) {}
 
   set(res: Response, token: string): void {
-    const maxAge = ms(this.configService.getOrThrow<string>('JWT_EXPIRES_IN') as StringValue)
+    const expiresIn = this.configService.getOrThrow<string>('JWT_EXPIRES_IN')
+    const maxAge: number = (ms as unknown as (v: string) => number)(expiresIn)
     res.cookie(COOKIE_NAME, token, {
       httpOnly: true,
       secure: this.configService.get<string>('NODE_ENV') === 'production',
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,12 +3,15 @@ import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
+
+  app.use(cookieParser())
 
   app.enableCors({
     origin: configService.getOrThrow<string>('CORS_ORIGIN'),
